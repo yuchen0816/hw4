418ce9a18e0001a1f2fea31226643e02920cdcd3
yuchen0816 <kevinxiao0816@icloud.com>
2021-06-15 02:06:38 -0700

    add main.cpp

---
+++ hw4_2/main.cpp
@@ -0,0 +1,27 @@
+#include "mbed.h"
+#include "bbcar.h"
+#include "bbcar_rpc.h"
+
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+BBCar car(pin5, pin6, servo_ticker);
+BufferedSerial uart(D1,D0); //tx,rx
+//BufferedSerial xbee(D1, D0);
+
+int main() {
+   char buf[256], outbuf[256];
+   FILE *devin = fdopen(&uart, "r");
+   FILE *devout = fdopen(&uart, "w");
+   while (1) {
+      memset(buf, 0, 256);
+      for( int i = 0; ; i++ ) {
+         char recv = fgetc(devin);
+         if(recv == '\n') {
+            printf("\r\n");
+            break;
+         }
+         buf[i] = fputc(recv, devout);
+      }
+   RPC::call(buf, outbuf);
+   }
+}
